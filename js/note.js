07df3ea25c32d511a489748c57a1a7da66c5d77e
Laurence042 <qq2120286535@gmail.com>
2020-07-20 07:43:36 +0800

    small fix

--- js/note.js
+++ js/note.js
@@ -35,14 +35,14 @@ let demo = new Vue({
         check_ddl() {
             let re = /- ([\d\.]+)(?: ([\d\:]+))?/g;
             let date_setters = [
-                (date_obj,day)=>{date_obj.setDate(day)},
-                (date_obj,month)=>{date_obj.setMonth(month-1)},
-                (date_obj,year)=>{date_obj.setYear(year)},
+                (date_obj, day) => { date_obj.setDate(day) },
+                (date_obj, month) => { date_obj.setMonth(month - 1) },
+                (date_obj, year) => { date_obj.setYear(year) },
             ]
             let time_setters = [
-                (date_obj,hour)=>{date_obj.setHours(hour)},
-                (date_obj,minute)=>{date_obj.setMinutes(minute)},
-                (date_obj,second)=>{date_obj.setSeconds(second)},
+                (date_obj, hour) => { date_obj.setHours(hour) },
+                (date_obj, minute) => { date_obj.setMinutes(minute) },
+                (date_obj, second) => { date_obj.setSeconds(second) },
             ]
 
 
@@ -51,23 +51,23 @@ let demo = new Vue({
             while (first_date_match = re.exec(this.raw_markdown)) {
                 let today = new Date();
                 let this_ddl = new Date(0);
-                this_ddl.setFullYear(today.getFullYear(),today.getMonth(),today.getDate());
+                this_ddl.setFullYear(today.getFullYear(), today.getMonth(), today.getDate());
 
                 let raw_date = first_date_match[1];
                 raw_date = raw_date.split(".").map(parseFloat);
                 raw_date = raw_date.reverse();
 
-                for(let i in raw_date){
-                    date_setters[i](this_ddl,raw_date[i])
+                for (let i in raw_date) {
+                    date_setters[i](this_ddl, raw_date[i])
                 }
-                
+
                 let raw_time = first_date_match[2];
-                if(raw_time!=null){
+                if (raw_time != null) {
                     raw_time = raw_time.split(":").map(parseFloat);
-                    for(let i in raw_time){
-                        time_setters[i](this_ddl,raw_time[i]);
+                    for (let i in raw_time) {
+                        time_setters[i](this_ddl, raw_time[i]);
                     }
-                }else{
+                } else {
                     this_ddl.setHours(8);
                 }
                 first_date_ls.push(this_ddl);
@@ -85,36 +85,40 @@ let demo = new Vue({
             let first_ddl = this.ddl[0];
             this.danger = false;
 
-            let prefix = "<h1>Deadline: ";
+            let prefix = ""
+            prefix += "<h2>今天：</br>" + today.toLocaleDateString() + "</h2>"
+
             if (first_ddl.getTime() == new Date(0).getTime()) {
-                prefix += "No ddl!</h1>";
-            } else if (first_ddl - today <= (2* 24 * 3600 * 1000)) {
-                if (first_ddl - today >= 0) {
-                    let totalMiliSecond = first_ddl.getTime() - today.getTime();
-                    let days = Math.floor(totalMiliSecond / (24 * 3600 * 1000));
-
-                    totalMiliSecond = totalMiliSecond % (24 * 3600 * 1000);
-                    let hours = Math.floor(totalMiliSecond / (3600 * 1000));
-
-                    totalMiliSecond = totalMiliSecond % (3600 * 1000);
-                    let minutes = Math.floor(totalMiliSecond / (60 * 1000));
-
-                    totalMiliSecond = totalMiliSecond % (60 * 1000);
-                    let seconds = Math.round(totalMiliSecond / 1000);
-
-                    prefix += days + ":" + hours + ":" + minutes + ":" + seconds + "</h1>";
-                    prefix += "<h1>Warning, ddl in 2 days</h1>";
-
-                    this.danger = true;
-                }
-                else {
-                    prefix += "</h1>";
-                    prefix += "<h1>Did you miss the ddl at " + first_ddl.toString() + "?</h1>";
-
-                    this.danger = true;
-                }
+                prefix += "<h1>No ddl!</h1>";
             } else {
-                prefix += "<h1>" + Math.ceil((first_ddl - today) / 86400000) + " days</h1>";
+                prefix += "<h2>最近的ddl：</br>" + first_ddl.toLocaleString() + "</h2>"
+                if (first_ddl - today <= (2 * 24 * 3600 * 1000)) {
+
+                    if (first_ddl - today >= 0) {
+                        let totalMiliSecond = first_ddl.getTime() - today.getTime();
+                        let days = Math.floor(totalMiliSecond / (24 * 3600 * 1000));
+
+                        totalMiliSecond = totalMiliSecond % (24 * 3600 * 1000);
+                        let hours = Math.floor(totalMiliSecond / (3600 * 1000));
+
+                        totalMiliSecond = totalMiliSecond % (3600 * 1000);
+                        let minutes = Math.floor(totalMiliSecond / (60 * 1000));
+
+                        totalMiliSecond = totalMiliSecond % (60 * 1000);
+                        let seconds = Math.round(totalMiliSecond / 1000);
+
+                        prefix += "<h1>Deadline: " + days + ":" + hours + ":" + minutes + ":" + seconds + "</h1>";
+
+                        this.danger = true;
+                    }
+                    else {
+                        prefix += "<h1>你是否错过了" + first_ddl.toLocaleString() + "的ddl？</h1>";
+
+                        this.danger = true;
+                    }
+                } else {
+                    prefix += "<h1>距离ddl还有" + Math.ceil((first_ddl - today) / 86400000) + " 天</h1>";
+                }
             }
 
             this.final_markdown = prefix + this.parsed_markdown;
